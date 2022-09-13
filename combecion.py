29980f4fc6e6a5a8019541604958c4df816d1261
Edwin <edwin.nol2.123@gmail.com>
2022-09-13 10:42:02 -0500

    act dia 13

---
+++ combecion.py
@@ -0,0 +1,5 @@
+dolar =input("ingresa tu cantidd de dolares: ")
+#age =input("Ingresa tu edead:")
+#print(f" yo soy {name}  y tengo {age}años")
+future= int(dolar)*20.99
+print(f"TIenes {future} pesos")
