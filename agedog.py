a12e501a816eb8a5ccdeef05d1829e79a222ca7e
Edwin <edwin.nol2.123@gmail.com>
2022-09-14 11:06:59 -0500

    dia 14

---
+++ agedog.py
@@ -0,0 +1,11 @@
+#lee la edad de alguien 
+años =input("ingresa tu edad: ")
+#Resta2 a esa edad y se le llama last_years
+last_years =int(años)-2 
+#first_years sera igual a los dos x 10.5
+first_years = 2*10.5
+#Multiplica los años restantes por 4
+años_perro = last_years*4 +first_years
+#suma ambos resultados
+#inprime ls siguiente "Tienes 24 años que equivalen a 78 años perrunos "
+print(f"TIenes {años} años que equivalen a {años_perro} años perrunos")
